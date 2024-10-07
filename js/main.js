ce402a87ea58a78254814807944b6989679bf5be
Elijah Jeruh <elijahgaskinsgeller@gmail.com>
2024-10-07 06:44:04 -0700

    moved three

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 
 
-import * as THREE from '../node_modules/three/build/three.module.js';
+//import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from './three.module.js';
 
 
 function WindowScrollNormalPosition() {
